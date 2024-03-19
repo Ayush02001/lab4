deba7781921c7ba4929abef7dcca7c7656380745
Ayush02001 <85024965+Ayush02001@users.noreply.github.com>
2024-03-19 05:46:20 -0400

    Update my-flask-app.py

--- my-flask-app.py
+++ my-flask-app.py
@@ -5,7 +5,4 @@ app = Flask(_name_)
 def hello_cloud():
   return 'Hello Cloud!'
 
-if __name__ == '__main__':
-    # Get port from environment variable, default to 8080
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0')
